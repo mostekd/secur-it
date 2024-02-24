4b51b5fe4d30028b6ffd10746d023f1a09b06084
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-02-24 18:40:22 +0100

    Add files via upload

--- test_firma (1).sql
+++ test_firma (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2024 at 09:32 PM
+-- Generation Time: Feb 24, 2024 at 06:39 PM
 -- Wersja serwera: 10.4.32-MariaDB
 -- Wersja PHP: 8.2.12
 
@@ -280,7 +280,7 @@ ALTER TABLE `formularz`
 -- AUTO_INCREMENT for table `formularz`
 --
 ALTER TABLE `formularz`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
