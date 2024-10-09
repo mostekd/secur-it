08000df3bf60f640ad904a2e8b68c79347dfd848
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-09 18:37:53 +0200

    update

--- DB/db_numery_kierunkowe.php
+++ DB/db_numery_kierunkowe.php
@@ -2,7 +2,7 @@
     include("db_connection.php");
     class db_numery_kierunkowe extends db_connection{
         function selectNrKierunkowe(){
-            $query = 'SELECT * FROM numery_kierunkowe';
+            $query = 'SELECT * FROM numery_kierunkowe WHERE 1';
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0){
             return $data;
