3bdfd0eb885885d2d3e3ddc84751b3d2a5935ee9
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-10 20:00:54 +0200

    update

--- DB/db_konta.php
+++ DB/db_konta.php
@@ -10,7 +10,7 @@
         }
 
         function insertKonto ($imie, $nazwisko, $PESEL, $email, $comments){
-            $query = "INSERT INTO `konta`(`id_konto`, `id_administrator`, `id_pracownik`, `id_klient`, `imie`, `nazwisko`, `id_nick`, `adres_e_mail`, `id_numer_kierunkowy`, `numer_telefonu`, `login`, `haslo`) VALUE ('".$imie."','".$nazwisko."','".$email."','".$email."','".$comments."');";
+            $query = "INSERT INTO `konta`(`id_konto`, `id_administrator`, `id_pracownik`, `id_klient`, `imie`, `nazwisko`, `id_nick`, `adres_e_mail`, `id_numer_kierunkowy`, `numer_telefonu`, `haslo`) VALUE ('".$imie."','".$nazwisko."','".$email."','".$email."','".$comments."');";
             $data = mysqli_query($this->connect, $query);
             header('location: ../BO/student_list.php'); 
             $this->close();
@@ -25,7 +25,7 @@
         }
 
         function updateKonto ($id_ucznia, $imie, $nazwisko, $PESEL, $email, $comments){
-            $query = "UPDATE `uczen` SET `id_administrator`='".$id_administrator."',`id_pracownik`='".$id_pracownik."',`id_klient`='".$id_klient."',`imie`='".$imie."',`nazwisko`='".$nazwisko."' WHERE `id_konto`=".$id_konto.";";
+            $query = "UPDATE `uczen` SET `id_administrator`='".$id_administrator."',`id_pracownik`='".$id_pracownik."',`id_klient`='".$id_klient."',`imie`='".$imie."',`nazwisko`='".$nazwisko."',`id_nick`='".$id_nick."',`adres_e_mail`='".$adres_e_mail."',`id_numer_kierunkowy`='".$id_numer_kierunkowy."',`numer_telefonu`='".$numer_telefonu."',`haslo`='".$haslo."' WHERE `id_konto`=".$id_konto.";";
             $data = mysqli_query($this->connect, $query);
             unset($_GET['id']);
             header('location: ../BO/student_list.php');   
