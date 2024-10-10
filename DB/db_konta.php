5c3f7e293fa4ca81da9ed6cd06a6127ca020406b
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-10 14:45:31 +0200

    update

--- DB/db_konta.php
+++ DB/db_konta.php
@@ -2,7 +2,7 @@
     include("db_connection.php");
     class db_konta extends db_connection{
         function selectKonto(){
-            $query = "SELECT * FROM `formularz_kontaktowy` WHERE login='$username' AND haslo='$encrypted'";
+            $query = "SELECT * FROM `konta` WHERE login='$username' AND haslo='$encrypted'";
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0){
             return $data;
@@ -10,7 +10,7 @@
         }
 
         function insertKonto ($imie, $nazwisko, $PESEL, $email, $comments){
-            $query = "INSERT INTO `formularz_kontaktowy`(`imie`, `nazwisko`, `e_mail`, `numer_telefonu`, `tytul`, `wiadomosc`) VALUES ('".$imie."','".$nazwisko."','".$email."','".$email."','".$comments."');";
+            $query = "INSERT INTO `konta`(`id_konto`, `id_administrator`, `id_pracownik`, `id_klient`, `imie`, `nazwisko`, `id_nick`, `adres_e_mail`, `id_numer_kierunkowy`, `numer_telefonu`, `login`, `haslo`) VALUE ('".$imie."','".$nazwisko."','".$email."','".$email."','".$comments."');";
             $data = mysqli_query($this->connect, $query);
             header('location: ../BO/student_list.php'); 
             $this->close();
