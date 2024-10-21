39f0498840edffa74ca57f8d9a3baf5a59cb8fd4
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-21 12:06:58 +0200

    update

---
+++ FO/header.php
@@ -0,0 +1,17 @@
+<header>
+            <a href="https://www.facebook.com/profile.php?id=61554027179435" class="spolecznosci" target="_blank">
+                <i class="fa-brands fa-facebook" style="color: #51ca52;"></i>
+            </a>
+            <a href="#ig" class="spolecznosci" target="_blank">
+                <i class="fa-brands fa-instagram" style="color: #51ca52;"></i>
+            </a>
+            <a href="#yt" class="spolecznosci" target="_blank">
+                <i class="fa-brands fa-youtube" style="color: #51ca52;"></i>
+            </a>
+            <a href="https://www.linkedin.com/company/secur-it2/" class="spolecznosci" target="_blank">
+                <i class="fa-brands fa-linkedin" style="color: #000000;"></i>
+            </a>
+            <a href="#github" class="spolecznosci" target="_blank">
+                <i class="fa-brands fa-github" style="color: #000000;"></i>
+            </a>
+        </header>
