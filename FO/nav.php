dbc388a817745410dd0dbdba7f9c2601df8df129
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-24 19:59:37 +0200

    update

--- FO/nav.php
+++ FO/nav.php
@@ -36,7 +36,7 @@
                     </div>
                     <div class="dropdown">
                         <div class="dropdown-top">
-                             <div class="dropdown-logo" style="--i:1;--j:5"><a href=".konto.php"></a>Konto</div>
+                             <div class="dropdown-logo" style="--i:1;--j:5"><a href="./konto.php"></a>Konto</div>
                             <div class="dropdown-toggle">
                                 <i class="fa-solid fa-bars"></i>
                             </div>
