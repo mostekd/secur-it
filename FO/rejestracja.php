9873d17bba4d50232c8540ee1df4592c07f5d980
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-10-31 14:39:48 +0100

    update

--- FO/rejestracja.php
+++ FO/rejestracja.php
@@ -74,44 +74,22 @@
                         <label for="firma">Firma</label>
                         <input type="radio" id="osoba_publiczna" name="typ_konta" value="osoba_publiczna">
                         <label for="osoba_publiczna">Osoba publiczna</label>
-                    </div>
-                    <!-- Pole Nazwa firmy dla firm -->
-                    <div class="form-group" id="nazwa_firmy_group">
                         <label for="nazwa_firmy">Nazwa firmy:</label>
                         <input type="text" id="nazwa_firmy" name="nazwa_firmy" placeholder="Wpisz nazwę firmy">
-                    </div>
-                    <div class="form-group" id="nazwa_firmy_cd_group">
                         <label for="nazwa_firmy">Nazwa firmy c.d.:</label>
                         <input type="text" id="nazwa_firmy_cd" name="nazwa_firmy_cd" placeholder="Wpisz nazwę firmy">
-                    </div>
-
-                    <!-- Pole NIP dla firm -->
-                    <div class="form-group" id="nip_group">
                         <label for="nip">NIP:</label>
                         <input type="text" id="nip" name="nip" placeholder="Wpisz NIP firmy">
-                    </div>
-                    <!-- Pole Imię dla osoby publicznej -->
-                    <div class="form-group" id="imie_group">
                         <label for="imie">Imię:</label>
                         <input type="text" id="imie" name="imie" placeholder="Wpisz imię">
-                    </div>
-                    <!-- Pole Nazwisko dla osoby publicznej -->
-                    <div class="form-group" id="nazwisko_group">
                         <label for="nazwisko">Nazwisko:</label>
                         <input type="text" id="nazwisko" name="nazwisko" placeholder="Wpisz nazwisko">
-                    </div>
-                    <!-- Zawsze wypełniane -->
-                    <div class="form-group">
                         <label for="nick">Nazwa użytkownika (Nick):</label>
                         <input type="text" id="nick" name="nick" placeholder="Wpisz nazwę użytkownika" required>
-                    </div>
-
-                    <div class="form-group">
                         <label for="adres_e_mail">Email:</label>
                         <input type="email" id="adres_e_mail" name="adres_e_mail" placeholder="Wpisz email" required>
-                    </div>
-                    <div class="form_group">
                         <label for="numer_telefonu_firma" id="numer_telefonu_firma_txt" >Numer telefonu firmy:</label>
+                        
                         <?php
                             include('../DB/db_numery_kierunkowe.php');
                             $baza = new db_numery_kierunkowe();
@@ -125,24 +103,27 @@
                             }
                             
                             $data = $baza->selectNrKierunkowe();
-                            if ($data){
-                                
-                            echo '<div class="phone_number">';
-                            echo '<select class="kierunkowy" name="id_numer_kierunkowy" default="">';
-                            while ($row = mysqli_fetch_assoc($data)){
-                                $text = '<option id="pole" class="kierunkowy"';
-                                if($row["id_numer_kierunkowy"] == $selectedId)
+                            if ($data)
+                            {
+                                echo '<div class="phone_number">';
+                                echo '<select class="kierunkowy" name="id_numer_kierunkowy" default="">';
+                                while ($row = mysqli_fetch_assoc($data))
                                 {
-                                $text .= 'selected = "selected"';
-                                } 
-                                $text .= ' value=' .$row["id_numer_kierunkowy"] .'> ' .$row["numer_kierunkowy"]. " " .$row["kraj"] .'</option>';
+                                    $text = '<option id="pole" class="kierunkowy"';
+                                    if($row["id_numer_kierunkowy"] == $selectedId)
+                                    {
+                                    $text .= 'selected = "selected"';
+                                    } 
+                                    $text .= ' value=' .$row["id_numer_kierunkowy"] .'> ' .$row["numer_kierunkowy"]. " " .$row["kraj"] .'</option>';
 
-                                echo $text;
-                            }
+                                    echo $text;
+                                }
                                 echo '</select>';
 
                                 mysqli_free_result($data);
-                            } else {
+                            } 
+                            else 
+                            {
                                 echo "Błąd zapytania: " .mysqli_error($connect);
                             }
 
@@ -151,13 +132,8 @@
                         ?>
                         <input type="text" id="numer_telefonu" name="numer_telefonu" placeholder="Wpisz numer telefonu" required>
                         <input type="text" id="numer_telefonu_firma" name="numer_telefonu_firma" placeholder="Wpisz numer telefonu firmy" required>
-                    </div>
-                    <div class="form-group">
                         <label for="haslo">Hasło:</label>
                         <input type="password" id="haslo" name="haslo" placeholder="Wpisz hasło" required>
-                    </div>
-
-                    <div class="form-group">
                         <button class="button" type="submit">Zarejestruj użytkownika</button>
                     </div>
                 </form>
