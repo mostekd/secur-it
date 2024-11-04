c1c5ec6237fb6993c00372183d520098a111cd8e
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-04 21:01:16 +0100

    update

--- FO/konto.php
+++ FO/konto.php
@@ -34,7 +34,7 @@
                     echo "<p>Imię: " . htmlspecialchars($user['imie']) . "</p>";
                     echo "<p>Nazwisko: " . htmlspecialchars($user['nazwisko']) . "</p>";
                     echo "<p>Email: " . htmlspecialchars($user['adres_e_mail']) . "</p>";
-                    echo "<p>Numer telefonu: " . htmlspecialchars($user['numer_kierunkowy'],$user['numer_telefonu']) . "</p>";
+                    echo "<p>Numer telefonu: " . htmlspecialchars($user['id_numer_kierunkowy'],$user['numer_telefonu']) . "</p>";
                     echo "<a href='./wyloguj.php' class='button'>Wyloguj się</a>";
                 } else {
                     echo "<p>Nie znaleziono danych użytkownika.</p>";
