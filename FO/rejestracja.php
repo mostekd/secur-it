ea3430afd0907dbb0cd5d735a89b2fd5c10b94db
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-04 18:07:34 +0100

    update

--- FO/rejestracja.php
+++ FO/rejestracja.php
@@ -19,7 +19,7 @@
         $haslo = sha1($_POST['haslo']);
 
         // Obsługa zapisu danych do bazy
-        $return = $baza->rejestrujKlienta($nazwa_firmy, $nazwa_cd, $nip, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail, $imie, $nazwisko, $nick, $haslo);
+        $return = $baza->rejestrujKlienta($nazwa_firmy, $nazwa_cd, $nip, $id_numer_kierunkowy, $numer_telefonu_firma, $numer_telefonu, $adres_e_mail, $imie, $nazwisko, $nick, $haslo);
         if(isset($return)){
             switch($return) {
                 case 1:
@@ -74,21 +74,74 @@
                         <input type="radio" id="osoba_publiczna" name="typ_konta" value="osoba_publiczna">
                         <label for="osoba_publiczna">Osoba publiczna</label>
                         <div class="firma">
-                        <label for="nazwa_firmy">Nazwa firmy:</label>
-                        <input type="text" id="nazwa_firmy" name="nazwa_firmy" placeholder="Wpisz nazwę firmy">
+                            <label for="nazwa_firmy">Nazwa firmy:</label>
+                            <input type="text" id="nazwa_firmy" name="nazwa_firmy" placeholder="Wpisz nazwę firmy">
+                            <br>
+                            <label for="nazwa_firmy">Nazwa firmy c.d.:</label>
+                            <input type="text" id="nazwa_firmy_cd" name="nazwa_firmy_cd" placeholder="Wpisz nazwę firmy">
+                            <br>
+                            <label for="nip">NIP:</label>
+                            <input type="text" id="nip" name="nip" placeholder="Wpisz NIP firmy">
+                            <br>
+                            <label for="adres_e_mail_firma">Email firmy:</label>
+                            <input type="email" id="adres_e_mail_firma" name="adres_e_mail_firma" placeholder="Wpisz email firmy" required>
+                            <br>
+                            <label for="numer_telefonu_firma" id="numer_telefonu_firma_txt" >Numer telefonu firmy:</label>
+                            <?php
+                                include('../DB/db_numery_kierunkowe.php');
+                                $baza = new db_numery_kierunkowe();
+                                $baza->databaseConnect();
+                                
+                                $dataPolska = $baza->selectNrKierunkowePolska();
+                                if ($dataPolska){
+                                    while ($row = mysqli_fetch_assoc($dataPolska)){
+                                        $selectedId = $row["id_numer_kierunkowy"];
+                                    } 
+                                }
+                                
+                                $data = $baza->selectNrKierunkowe();
+                                if ($data)
+                                {
+                                    echo '<div class="phone_number">';
+                                    echo '<select class="kierunkowy" name="id_numer_kierunkowy" default="">';
+                                    while ($row = mysqli_fetch_assoc($data))
+                                    {
+                                        $text = '<option id="pole" class="kierunkowy"';
+                                        if($row["id_numer_kierunkowy"] == $selectedId)
+                                        {
+                                        $text .= 'selected = "selected"';
+                                        } 
+                                        $text .= ' value=' .$row["id_numer_kierunkowy"] .'> ' .$row["numer_kierunkowy"]. " " .$row["kraj"] .'</option>';
+
+                                        echo $text;
+                                    }
+                                    echo '</select>';
+                                    echo '</div>';
+                                    mysqli_free_result($data);
+                                } 
+                                else 
+                                {
+                                    echo "Błąd zapytania: " .mysqli_error($connect);
+                                }
+                                $baza->close();
+                            ?>
+                            <input type="text" id="numer_telefonu_firma" name="numer_telefonu_firma" placeholder="Wpisz numer telefonu firmy" required>  
+                        </div>
                         <br>
-                        <label for="nazwa_firmy">Nazwa firmy c.d.:</label>
-                        <input type="text" id="nazwa_firmy_cd" name="nazwa_firmy_cd" placeholder="Wpisz nazwę firmy">
+                        <label for="imie">Imię:</label>
+                        <input type="text" id="imie" name="imie" placeholder="Wpisz imię">
                         <br>
-                        <label for="nip">NIP:</label>
-                        <input type="text" id="nip" name="nip" placeholder="Wpisz NIP firmy">
+                        <label for="nazwisko">Nazwisko:</label>
+                        <input type="text" id="nazwisko" name="nazwisko" placeholder="Wpisz nazwisko">
                         <br>
-                        <label for="adres_e_mail_firma">Email firmy:</label>
-                        <input type="email" id="adres_e_mail_firma" name="adres_e_mail_firma" placeholder="Wpisz email firmy" required>
+                        <label for="nick">Nazwa użytkownika (Nick):</label>
+                        <input type="text" id="nick" name="nick" placeholder="Wpisz nazwę użytkownika" required>
                         <br>
-                        <label for="numer_telefonu_firma" id="numer_telefonu_firma_txt" >Numer telefonu firmy:</label>
+                        <label for="adres_e_mail">Email:</label>
+                        <input type="email" id="adres_e_mail" name="adres_e_mail" placeholder="Wpisz email" required>
+                        <br>
+                        <label for="numer_telefonu" id="numer_telefonu_txt" >Numer telefonu:</label>
                         <?php
-                            include('../DB/db_numery_kierunkowe.php');
                             $baza = new db_numery_kierunkowe();
                             $baza->databaseConnect();
                             
@@ -116,80 +169,22 @@
                                     echo $text;
                                 }
                                 echo '</select>';
-
+                                echo '</div>';
                                 mysqli_free_result($data);
                             } 
                             else 
                             {
                                 echo "Błąd zapytania: " .mysqli_error($connect);
                             }
-
-                            
                             $baza->close();
                         ?>
-                        <input type="text" id="numer_telefonu_firma" name="numer_telefonu_firma" placeholder="Wpisz numer telefonu firmy" required>
-                        </div>
+                        <input type="text" id="numer_telefonu" name="numer_telefonu" placeholder="Wpisz numer telefonu:" required>
+                        
+                        <br>
+                        <label for="haslo">Hasło:</label>
+                        <input type="password" id="haslo" name="haslo" placeholder="Wpisz hasło" required><br>
+                        <button class="button" type="submit">Zarejestruj użytkownika</button>
                     </div>
-                    <br>
-                    <label for="imie">Imię:</label>
-                    <input type="text" id="imie" name="imie" placeholder="Wpisz imię">
-                    <br>
-                    <label for="nazwisko">Nazwisko:</label>
-                    <input type="text" id="nazwisko" name="nazwisko" placeholder="Wpisz nazwisko">
-                    <br>
-                    <label for="nick">Nazwa użytkownika (Nick):</label>
-                    <input type="text" id="nick" name="nick" placeholder="Wpisz nazwę użytkownika" required>
-                    <br>
-                    <label for="adres_e_mail">Email:</label>
-                    <input type="email" id="adres_e_mail" name="adres_e_mail" placeholder="Wpisz email" required>
-                    <br>
-                    <label for="numer_telefonu" id="numer_telefonu_txt" >Numer telefonu:</label>
-                    <?php
-                        $baza = new db_numery_kierunkowe();
-                        $baza->databaseConnect();
-                        
-                        $dataPolska = $baza->selectNrKierunkowePolska();
-                        if ($dataPolska){
-                            while ($row = mysqli_fetch_assoc($dataPolska)){
-                                $selectedId = $row["id_numer_kierunkowy"];
-                            } 
-                        }
-                        
-                        $data = $baza->selectNrKierunkowe();
-                        if ($data)
-                        {
-                            echo '<div class="phone_number">';
-                            echo '<select class="kierunkowy" name="id_numer_kierunkowy" default="">';
-                            while ($row = mysqli_fetch_assoc($data))
-                            {
-                                $text = '<option id="pole" class="kierunkowy"';
-                                if($row["id_numer_kierunkowy"] == $selectedId)
-                                {
-                                $text .= 'selected = "selected"';
-                                } 
-                                $text .= ' value=' .$row["id_numer_kierunkowy"] .'> ' .$row["numer_kierunkowy"]. " " .$row["kraj"] .'</option>';
-
-                                echo $text;
-                            }
-                            echo '</select>';
-
-                            mysqli_free_result($data);
-                        } 
-                        else 
-                        {
-                            echo "Błąd zapytania: " .mysqli_error($connect);
-                        }
-
-                        
-                        $baza->close();
-                    ?>
-                    <input type="text" id="numer_telefonu" name="numer_telefonu" placeholder="Wpisz numer telefonu:" required>
-                    </div>
-                    <br>
-                    <label for="haslo">Hasło:</label>
-                    <input type="password" id="haslo" name="haslo" placeholder="Wpisz hasło" required>
-                    <br>
-                    <button class="button" type="submit">Zarejestruj użytkownika</button>
                 </form>
             </div>
         </main>
