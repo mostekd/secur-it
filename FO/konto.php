c8b4e9a08fe26c5d3f4e1c5ef449451cf10015d2
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-05 09:56:16 +0100

    update

--- FO/konto.php
+++ FO/konto.php
@@ -32,16 +32,19 @@
 
                 if ($data && mysqli_num_rows($data) > 0) {
                     $user = mysqli_fetch_assoc($data);
+                    echo "<div class='user_page'>";
                     echo "<h2>Witaj, " . htmlspecialchars($user['nick']) . "!</h2>";
                     echo "<p>Imię: " . htmlspecialchars($user['imie']) . "</p>";
                     echo "<p>Nazwisko: " . htmlspecialchars($user['nazwisko']) . "</p>";
                     echo "<p>Email: " . htmlspecialchars($user['uae']) . "</p>";
                     echo "<p>Numer telefonu: " . htmlspecialchars($user['unk']) . " " . htmlspecialchars($user['unt']) . "</p>";
-                    echo "<h4>Twoja Firma:</h4>";
+                    echo "</div><div class='firma_page'>";
+                    echo "<h2>Twoja Firma:</h2>";
                     echo "<p>Nazwa firmy:  " . htmlspecialchars($user['nazwa']) . " " . htmlspecialchars($user['nazwa_cd']) . "</p>";
-                    echo "<p>" . htmlspecialchars($user['nip']) . "</p>";
-                    echo "<p>Numer telefonu: " . htmlspecialchars($user['fnk']) . " " . htmlspecialchars($user['fnt']) . "</p>";
+                    echo "<p>NIP: " . htmlspecialchars($user['nip']) . "</p>";
+                    echo "<p>Numer telefonu firmy: " . htmlspecialchars($user['fnk']) . " " . htmlspecialchars($user['fnt']) . "</p>";
                     echo "<p>Email firmy: " . htmlspecialchars($user['fae']) . "</p>";
+                    echo"</div>";
                     echo "<a href='./wyloguj.php' class='button'>Wyloguj się</a>";
                 } else {
                     echo "<p>Nie znaleziono danych użytkownika.</p>";
