26b587d656ec2e28f9ef6d12e1fa4012784789fe
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-12 16:25:59 +0100

    update db_firmy.php, edytuj_firme.php
    dodano funkcje usuń, edytuj do db_firmy.
    
    utworzono podstronę edytuj_firmę w której administrator firmy ma możliwośc zmienienia jej danych

--- DB/db_firmy.php
+++ DB/db_firmy.php
@@ -16,10 +16,28 @@ class db_firmy extends db_connection
         function selectFirmaById($id_firma)
         {
             $query = "SELECT * FROM `firmy` WHERE id_firma = ".$id_firma.";";
+            $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0) 
             {
                 return $data;
             }
+        }
+
+        function deleteFirmaById($id_firma)
+        {
+            $query = "Delete from firmy where id_firma =".$id_firma.";";
+            $data = mysqli_query($this->connect, $query);
+            unset($_GET['id']);
+            $this->close();
+        }
+
+        function updateFirma($id_firma, $nazwa, $nazwacd, $nip, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail)
+        {
+            $query = "UPDATE `firmy` SET `nazwa`='".$nazwa."',`nazwa_cd`='".$nazwacd."',`nip`='".$nip."',`id_numer_kierunkowy`='".$id_numer_kierunkowy."',`numer_telefonu`='".$numer_telefonu."',`adres_e_mail`=''".$adres_e_mail."' WHERE `id_firma`=".$id_firma.";";
+            $data = mysqli_query($this->connect, $query);
+            unset($_GET['id']);
+            header('location: ../index_admin.php');
+            $this->close();
         }
     }
 ?>
---
+++ FO/edytuj_firme.php
@@ -0,0 +1,69 @@
+<?php
+include('../DB/db_firmy.php');
+session_start();
+
+$baza = new db_firmy();
+
+if (isset($_GET['id_firma'])) {
+    $id_firma = $_GET['id_firma'];
+    $result = $baza->selectFirmaById($id_firma);
+    $firma = mysqli_fetch_assoc($result);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['update'])) {
+    $nazwa = $_POST['nazwa'];
+    $nazwacd = $_POST['nazwa_cd'];
+    $nip = $_POST['nip'];
+    $id_numer_kierunkowy = $_POST['id_numer_kierunkowy'];
+    $numer_telefonu = $_POST['numer_telefonu'];
+    $adres_e_mail = $_POST['adres_e_mail'];
+
+    $baza->updateFirma($id_firma, $nazwa, $nazwacd, $nip, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail);
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./script.js" defer></script>
+    <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="../images/ikona.png">
+    <title>Secur IT | Edytuj Dane Firmy</title>
+</head>
+<body>
+    <div class="tlo"></div>
+    <main class="main">
+        <?php include("header.php"); ?>
+        <?php include("nav.php"); ?>
+        
+        <section class="form-section">
+            <h2>Edytuj Dane Firmy</h2>
+            <form action="edytuj_firme.php?id_firma=<?php echo $id_firma; ?>" method="post">
+                <label for="nazwa">Nazwa firmy:</label>
+                <input type="text" id="nazwa" name="nazwa" value="<?php echo $firma['nazwa']; ?>" required>
+
+                <label for="nazwa_cd">Nazwa dodatkowa:</label>
+                <input type="text" id="nazwa_cd" name="nazwa_cd" value="<?php echo $firma['nazwa_cd']; ?>" required>
+
+                <label for="nip">NIP:</label>
+                <input type="text" id="nip" name="nip" value="<?php echo $firma['nip']; ?>" required>
+
+                <label for="id_numer_kierunkowy">Numer kierunkowy:</label>
+                <input type="text" id="id_numer_kierunkowy" name="id_numer_kierunkowy" value="<?php echo $firma['id_numer_kierunkowy']; ?>" required>
+
+                <label for="numer_telefonu">Numer telefonu:</label>
+                <input type="text" id="numer_telefonu" name="numer_telefonu" value="<?php echo $firma['numer_telefonu']; ?>" required>
+
+                <label for="adres_e_mail">Adres e-mail:</label>
+                <input type="email" id="adres_e_mail" name="adres_e_mail" value="<?php echo $firma['adres_e_mail']; ?>" required>
+
+                <button type="submit" name="update">Zaktualizuj dane</button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
