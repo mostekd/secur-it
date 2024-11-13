4404a1a2ed2b547a71c964746ff0435e1d247d4d
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-13 18:04:17 +0100

    Update admin_nav.php

--- BO/admin_nav.php
+++ BO/admin_nav.php
@@ -42,10 +42,9 @@
                             </div>
                         </div>
                         <ol class="dropdown-list">
-                            <li class="dropdown-item" style="--i:1;--j:5"><a href="./admin_konto.php"></a>Konto</li>
-                            <li class="dropdown-item" style="--i:1;--j:5"><a href="../FO/logowanie.php"></a>Logowanie</li>
-                            <li class="dropdown-item" style="--i:2;--j:4"><a href="../FO/rejestracja.php"></a>Rejestracja</li>
-                        </ol>
+                            <li class="dropdown-item" style="--i:1;--j:3"><a href="./admin_konto.php"></a>Konto</li>
+                            <li class="dropdown-item" style="--i:3;--j:2"><a href="../FO/index.php"></a>Strona Główna</li>
+                            </ol>
                     </div>
                 </div>
                 <div class="phone">
@@ -56,18 +55,16 @@
                             </div>
                         </div>
                         <ol class="dropdown-list">
-                            <li class="dropdown-item" style="--i:1;--j:11"><a href="./admin_o-firmie.php"></a>O Firmie</li>
-                            <li class="dropdown-item" style="--i:2;--j:12"><a href="./admin_pracownicy.php"></a>Pracownicy</li>
-                            <li class="dropdown-item" style="--i:3;--j:11"><a href="./admin_wpisy.php"></a>Wpisy</li>
-                            <li class="dropdown-item" style="--i:4;--j:10"><a href="./admin_sieci_komputerowe.php"></a>Sieci komputerowe</li>
-                            <li class="dropdown-item" style="--i:5;--j:9"><a href="./admin_systemy_operacyjne.php"></a>Systemy Operacyjne</li>
-                            <li class="dropdown-item" style="--i:6;--j:8"><a href="./admin_bazy_danych.php"></a>Bazy Danych</li>
-                            <li class="dropdown-item" style="--i:7;--j:7"><a href="./admin_strony_internetowe.php"></a>Strony Internetowe</li>
-                            <li class="dropdown-item" style="--i:8;--j:6"><a href="./admin_serwis_komputerowy.php"></a>Serwis Komputerowy</li>
-                            <li class="dropdown-item" style="--i:9;--j:5"><a href="./admin_kontakt.php"></a>Kontakt</li>
-                            <li class="dropdown-item" style="--i:10;--j:4"><a href="./admin_konto.php"></a>Konto</li>
-                            <li class="dropdown-item" style="--i:11;--j:2"> <a href="../FO/logowanie.php"></a>Logowanie</li>
-                            <li class="dropdown-item" style="--i:12;--j:1"> <a href="../FO/rejestracja.php"></a>Rejestracja</li>
+                            <li class="dropdown-item" style="--i:1;--j:10"><a href="./admin_o-firmie.php"></a>O Firmie</li>
+                            <li class="dropdown-item" style="--i:2;--j:9"><a href="./admin_pracownicy.php"></a>Pracownicy</li>
+                            <li class="dropdown-item" style="--i:3;--j:8"><a href="./admin_wpisy.php"></a>Wpisy</li>
+                            <li class="dropdown-item" style="--i:4;--j:7"><a href="./admin_sieci_komputerowe.php"></a>Sieci komputerowe</li>
+                            <li class="dropdown-item" style="--i:5;--j:6"><a href="./admin_systemy_operacyjne.php"></a>Systemy Operacyjne</li>
+                            <li class="dropdown-item" style="--i:6;--j:5"><a href="./admin_bazy_danych.php"></a>Bazy Danych</li>
+                            <li class="dropdown-item" style="--i:7;--j:4"><a href="./admin_strony_internetowe.php"></a>Strony Internetowe</li>
+                            <li class="dropdown-item" style="--i:8;--j:3"><a href="./admin_serwis_komputerowy.php"></a>Serwis Komputerowy</li>
+                            <li class="dropdown-item" style="--i:9;--j:2"><a href="./admin_kontakt.php"></a>Kontakt</li>
+                            <li class="dropdown-item" style="--i:10;--j:1"><a href="../FO/index.php"></a>Strona Główna</li>
                         </ol>
                     </div>
                 </div>
