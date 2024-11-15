f310a6e6f23fc3b7a1316fb541962ac39efba7e6
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-15 13:15:37 +0100

    Update admin_kontakt.php

--- BO/admin_kontakt.php
+++ BO/admin_kontakt.php
@@ -22,37 +22,49 @@
         ?>
         <main class="main">
             <h1>Lista Kontaktów</h1>
-            <?php if ($kontakty && mysqli_num_rows($kontakty) > 0): ?>
-                <table class="kontakt_table" border="1" cellpadding="15" cellspacing="0">
-                    <thead>
+            <?php 
+                if ($kontakty && mysqli_num_rows($kontakty) > 0): 
+            ?>
+            <table class="kontakt_table" border="1" cellpadding="15" cellspacing="0">
+                <thead>
+                    <tr>
+                        <th>Imię</th>
+                        <th>Nazwisko</th>
+                        <th>E-mail</th>
+                        <th>Numer Telefonu</th>
+                        <th>Tytuł</th>
+                        <th>Wiadomość</th>
+                        <th>Zgoda na przetwarzanie danych</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php 
+                        while ($row = mysqli_fetch_assoc($kontakty)): 
+                    ?>
                         <tr>
-                            <th>Imię</th>
-                            <th>Nazwisko</th>
-                            <th>E-mail</th>
-                            <th>Numer Telefonu</th>
-                            <th>Tytuł</th>
-                            <th>Wiadomość</th>
-                            <th>Zgoda na przetwarzanie danych</th>
+                            <td><?php echo htmlspecialchars($row['imie']); ?></td>
+                            <td><?php echo htmlspecialchars($row['nazwisko']); ?></td>
+                            <td><?php echo htmlspecialchars($row['e_mail']); ?></td>
+                            <td><?php echo htmlspecialchars($row['numer_kierunkowy']) .htmlspecialchars($row['numer_telefonu']); ?></td>
+                            <td><?php echo htmlspecialchars($row['tytul']); ?></td>
+                            <td><?php echo htmlspecialchars($row['wiadomosc']); ?></td>
+                            <td><?php echo $row['czy_zgoda'] ? 'Tak' : 'Nie'; ?></td>
                         </tr>
-                    </thead>
-                    <tbody>
-                        <?php while ($row = mysqli_fetch_assoc($kontakty)): ?>
-                            <tr>
-                                <td><?php echo htmlspecialchars($row['imie']); ?></td>
-                                <td><?php echo htmlspecialchars($row['nazwisko']); ?></td>
-                                <td><?php echo htmlspecialchars($row['e_mail']); ?></td>
-                                <td><?php echo htmlspecialchars($row['numer_kierunkowy']) .htmlspecialchars($row['numer_telefonu']); ?></td>
-                                <td><?php echo htmlspecialchars($row['tytul']); ?></td>
-                                <td><?php echo htmlspecialchars($row['wiadomosc']); ?></td>
-                                <td><?php echo $row['czy_zgoda'] ? 'Tak' : 'Nie'; ?></td>
-                            </tr>
-                        <?php endwhile; ?>
-                    </tbody>
-                </table>
-            <?php else: ?>
+                    <?php 
+                        endwhile; 
+                    ?>
+                </tbody>
+            </table>
+            <?php 
+                else: 
+            ?>
                 <p>Brak danych do wyświetlenia.</p>
-            <?php endif; ?>
-            <?php $baza->close(); ?>
+            <?php 
+                endif; 
+            ?>
+            <?php 
+                $baza->close(); 
+            ?>
         </main>
     </body>
 </html>
