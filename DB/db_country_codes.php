20310015c0239029e772378d096b15dabd2dc147
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-18 11:20:50 +0100

    Update db_country_codes.php

--- DB/db_country_codes.php
+++ DB/db_country_codes.php
@@ -1,6 +1,6 @@
 <?php
     class db_country_codes extends db_connection {
-        function selectNrKierunkowe() {
+        function selectCountryCodes() {
             $query = 'SELECT * FROM country_codes WHERE 1';
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0) {
@@ -8,7 +8,7 @@
             }
         }
         
-        function selectNrKierunkowePolska() {
+        function selectCountryCodesPolska() {
             $query = 'SELECT * FROM country_codes WHERE country = "Polska"';
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0) {
