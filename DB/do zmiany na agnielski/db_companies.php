4cbf3ebb80ae43381d80455d64311b744debdf63
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-18 12:40:37 +0100

    update

--- DB/do zmiany na agnielski/db_companies.php
+++ DB/do zmiany na agnielski/db_companies.php
@@ -4,7 +4,7 @@ class db_companies extends db_connection
     {
         function selectFirmy()
         {
-            $query = "SELECT * FROM `firmy` WHERE 1";
+            $query = "SELECT * FROM `companies` WHERE 1";
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0)
             {
@@ -12,9 +12,9 @@ class db_companies extends db_connection
             }
         }
 
-        function selectFirmaById($id_firma)
+        function selectFirmaById($id_company)
         {
-            $query = "SELECT f.* FROM `firmy` AS f WHERE f.id_firma = ".$id_firma.";";
+            $query = "SELECT c.* FROM `companies` AS c WHERE c.id_company = ".$id_company.";";
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0) 
             {
@@ -22,18 +22,18 @@ class db_companies extends db_connection
             }
         }
 
-        function deleteFirmaById($id_firma)
+        function deleteFirmaById($id_company)
         {
-            $query = "Delete from firmy where id_firma =".$id_firma.";";
+            $query = "Delete from companies where id_company =".$id_company.";";
             $data = mysqli_query($this->connect, $query);
             $this->close();
         }
 
-        function updateFirma($id_firma, $nazwa, $nazwacd, $nip, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail)
+        function updateFirma($id_company, $name, $additional_name, $tax, $id_country_code, $numer_telefonu, $adres_e_mail)
         {
-            $query = "UPDATE `firmy` SET `nazwa`='".$nazwa."',`nazwa_cd`='".$nazwacd."',`nip`='".$nip."',`id_numer_kierunkowy`='".$id_numer_kierunkowy."',`numer_telefonu`='".$numer_telefonu."',`adres_e_mail`='".$adres_e_mail."' WHERE `id_firma`=".$id_firma.";";
+            $query = "UPDATE `companies` SET `name`='".$name."',`additional_name`='".$additional_name."',`tax`='".$tax."',`id_country_code`='".$id_country_code."',`phone_number`='".$phone_number."',`email_address`='".$email_address."' WHERE `id_company`=".$id_company.";";
             $data = mysqli_query($this->connect, $query);
-            header('location: ./konto.php');
+            header('location: ./account.php');
             $this->close();
         }
     }
