ce1688c5a43140f13782c7f9190b1eb6ed5c7a1e
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-18 20:08:50 +0100

    update

---
+++ BO/admin_account.php
@@ -0,0 +1,59 @@
+<?php
+    include('../DB/db_konta.php');
+    include_once ('../include/functions.php');
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./admin.js" defer></script>
+    <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="../images/ikona.png">
+    <title>Secur IT | Admin Konto</title>
+</head>
+<body>
+<body>
+    <div class="tlo"></div>
+    <main class="main">
+    <?php
+            include("header.php");
+            include("nav.php");
+
+            if (/*isset($_SESSION['loggedin']) && */ $_SESSION['loggedin'] === true) {
+                $id_user = $_SESSION['id_user'];
+
+                $baza = new db_accounts();
+                $baza->databaseConnect();
+                $data = $baza->selectCustomerById($id_user, null);
+
+                if ($data && mysqli_num_rows($data) > 0) {
+                    $user = mysqli_fetch_assoc($data);
+                    echo "<div class='user_page'>";
+                    echo "<h2>Witaj, " . ($user['username']) . "!</h2>";
+                    echo "<p>Imię: " . ($user['first_name']) . "</p>";
+                    echo "<p>Nazwisko: " . ($user['last_name']) . "</p>";
+                    echo "<p>Numer telefonu: " . ($user['ucc']) . " " . ($user['upn']) . "</p>";
+                    echo "<p>Email: " . ($user['uea']) . "</p>";                        
+                        // Dodanie opcji administracyjnych, jeśli użytkownik jest administratorem firmy
+                        if ($user['is_company_admin'] == 1) {
+                            echo "<a href='./add_employee.php'><button>Dodaj pracownika</button</a>";
+                            echo "<a href='./view_employee.php'><button>Wyświetl pracowników</botton></a>";
+                            echo "<a href='./edit_company.php'><button>Edytuj dane firmy</button></a>";
+                        }
+                        echo "</div>";
+                    }
+                    echo "<a href='./logout.php' class='button_logout'>Wyloguj się</a>";
+                } else {
+                    echo "<p>Nie znaleziono danych użytkownika.</p>";
+                }
+                $baza->close();
+            } else {
+                echo "<p>Musisz być <a href='./login.php'>zalogowany</a>, aby zobaczyć tę stronę.</p>";
+            }
+        ?>
+    </main>
+</body>
+</html>
