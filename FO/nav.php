282fef147616e3e636fe577633710b888d2bd747
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-18 14:24:51 +0100

    Update nav.php

--- FO/nav.php
+++ FO/nav.php
@@ -68,14 +68,14 @@ else {
                 </div>
             </div>
             <ol class="dropdown-list">
-                <li class="dropdown-item" style="--i:1;--j:10"><a href="./o_firmie.php"></a>O Firmie</li>
-                <li class="dropdown-item" style="--i:2;--j:9"><a href="./pracownicy.php"></a>Pracownicy</li>
-                <li class="dropdown-item" style="--i:3;--j:8"><a href="./wpisy.php"></a>Wpisy</li>
-                <li class="dropdown-item" style="--i:4;--j:7"><a href="./sieci_komputerowe.php"></a>Sieci komputerowe</li>
-                <li class="dropdown-item" style="--i:5;--j:6"><a href="./systemy_operacyjne.php"></a>Systemy Operacyjne</li>
-                <li class="dropdown-item" style="--i:6;--j:5"><a href="./bazy_danych.php"></a>Bazy Danych</li>
-                <li class="dropdown-item" style="--i:7;--j:4"><a href="./strony_internetowe.php"></a>Strony Internetowe</li>
-                <li class="dropdown-item" style="--i:8;--j:3"><a href="./serwis_komputerowy.php"></a>Serwis Komputerowy</li>
+                <li class="dropdown-item" style="--i:1;--j:10"><a href="./about_company.php"></a>O Firmie</li>
+                <li class="dropdown-item" style="--i:2;--j:9"><a href="./employess.php"></a>Pracownicy</li>
+                <li class="dropdown-item" style="--i:3;--j:8"><a href="./posts.php"></a>Wpisy</li>
+                <li class="dropdown-item" style="--i:4;--j:7"><a href="./networks.php"></a>Sieci komputerowe</li>
+                <li class="dropdown-item" style="--i:5;--j:6"><a href="./systems.php"></a>Systemy Operacyjne</li>
+                <li class="dropdown-item" style="--i:6;--j:5"><a href="./databaes.php"></a>Bazy Danych</li>
+                <li class="dropdown-item" style="--i:7;--j:4"><a href="./websites.php"></a>Strony Internetowe</li>
+                <li class="dropdown-item" style="--i:8;--j:3"><a href="./computer_service.php"></a>Serwis Komputerowy</li>
                 <li class="dropdown-item" style="--i:9;--j:2"> <a href="./contact.php"></a>Kontakt</li>
                 <?php
                     if($is_admin == 1){
