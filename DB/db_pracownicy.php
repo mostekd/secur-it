d15ecb55f97c480932dd51a908a7ecca47f3f418
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-18 11:13:30 +0100

    Update db_pracownicy.php

--- DB/db_pracownicy.php
+++ DB/db_pracownicy.php
@@ -1,16 +1,16 @@
 <?php    
 include_once("db_connection.php");
-class db_pracownicy extends db_connection{
-        function selectPracownikById($id_pracownik){
-            $query = "SELECT u.imie, u.nazwisko, u.id_numer_kierunkowy, nk.numer_kierunkowy, u.numer_telefonu, u.adres_e_mail, p.adres_zamieszkania, p.data_urodzenia, p.zdjecie, um.*, s.nazwa, d.nazwa_dzialu
-            FROM `uzytkownicy` AS u
-            LEFT JOIN numery_kierunkowe AS nk ON u.id_numer_kierunkowy = nk.id_numer_kierunkowy
-            LEFT JOIN pracownicy AS p ON u.id_pracownik = p.id_pracownik
-            LEFT JOIN umowy AS um ON p.id_umowa = um.id_umowa
-            LEFT JOIN lokalizacje AS l ON um.id_lokalizacja_pracy = l.id_lokalizacja
-            LEFT JOIN stanowiska AS s ON p.id_stanowisko = s.id_stanowisko
-            LEFT JOIN dzialy AS d ON um.id_dzial = d.id_dzial
-            WHERE u.id_pracownik = ".$id_pracownik;
+class db_employees extends db_connection{
+        function selectPracownikById($id_employee){
+            $query = "SELECT u.first_name, u.last_name, u.id_country_code, cc.id_country_code, u.phone_number, u.email_address, e.home_address, e.date_of_birth, e.photo, c.*, s.nazwa, d.nazwa_dzialu
+            FROM `users` AS u
+            LEFT JOIN country_codes AS cc ON u.id_country_code = cc.id_country_code
+            LEFT JOIN employees AS e ON u.id_employee = e.id_employee
+            LEFT JOIN contracts AS c ON e.id_contract = c.id_contract
+            LEFT JOIN locations AS l ON c.id_work_location = l.id_location
+            LEFT JOIN positions AS p ON e.id_position = p.id_position
+            LEFT JOIN departments AS d ON c.id_department = d.id_department
+            WHERE u.id_employee = ".$id_employee;
             $data = mysqli_query($this->connect, $query);
             
             if (mysqli_num_rows($data) > 0) {
@@ -18,12 +18,12 @@ class db_pracownicy extends db_connection{
             }
         }
         function selectPracownik(){
-            $query = 'SELECT u.id_pracownik, u.imie, u.nazwisko, p.zdjecie, s.nazwa, d.nazwa_dzialu 
-            FROM uzytkownicy AS u
-            JOIN pracownicy as p ON u.id_pracownik = p.id_pracownik
-            JOIN stanowiska AS s ON p.id_stanowisko = s.id_stanowisko 
-            JOIN umowy as um ON p.id_umowa = um.id_umowa
-            left JOIN dzialy AS d ON um.id_dzial = d.id_dzial';
+            $query = 'SELECT u.id_employee, u.first_name, u.last_name, e.photo, p.name, d.department_name 
+            FROM users AS u
+            JOIN employees as e ON u.id_employee = e.id_employee
+            JOIN positions AS p ON e.id_position = p.id_position 
+            JOIN contracts as c ON e.id_contract = c.id_contract
+            left JOIN departments AS d ON c.id_department = d.id_department';
 
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0){
@@ -31,34 +31,34 @@ class db_pracownicy extends db_connection{
             }
         }
         function selectPracownikAll(){
-            $query = 'SELECT * FROM `pracownicy`;';
+            $query = 'SELECT * FROM `employees`;';
             $data = mysqli_query($this->connect, $query);
             if (mysqli_num_rows($data) > 0){
             return $data;
             }
         }
 
-        function insertPracownik ($imie, $nazwisko, $adres_zamieszkania, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail, $PESEL, $numer_umowy, $numer_ubezpieczenia, $data_urodzenia, $data_zatrudnienia, $wynagrodzenie, $zdjecie, $id_stanowisko, $id_lokalizacja_pracy, $id_dzial, $id_samochod){
-            $query = "INSERT INTO `pracownicy`(`imie`, `nazwisko`, `adres_zamieszkania`, `id_numer_kierunkowy`, `numer_telefonu`, `adres_e_mail`, `PESEL`, `numer_umowy`, `numer_ubezpieczenia`, `data_urodzenia`, `data_zatrudnienia`, `wynagrodzenie`, `zdjecie`, `id_stanowisko`, `id_lokalizacja_pracy`, `id_dzial`, `id_samochod`) VALUES ('".$imie."','".$nazwisko."','".$adres_zamieszkania."','".$id_numer_kierunkowy."','".$numer_telefonu."','".$adres_e_mail."','".$PESEL."','".$numer_umowy."','".$numer_ubezpieczenia."','".$data_urodzenia."','".$data_zatrudnienia."','".$wynagrodzenie."','".$zdjecie."','".$id_stanowisko."','".$id_lokalizacja_pracy."','".$id_dzial."','".$id_samochod."',);";
-            $data = mysqli_query($this->connect, $query);
-            header('location: ../BO/pracownicy.php'); 
-            $this->close();
-        }
+        // function insertPracownik ($imie, $nazwisko, $adres_zamieszkania, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail, $PESEL, $numer_umowy, $numer_ubezpieczenia, $data_urodzenia, $data_zatrudnienia, $wynagrodzenie, $zdjecie, $id_stanowisko, $id_lokalizacja_pracy, $id_dzial, $id_samochod){
+        //     $query = "INSERT INTO `employees`(`home_address`, `id_contract`, `date_of_birth`, `photo`, `is_admin`, `id_position`, `id_car`) VALUES ('".$imie."','".$nazwisko."','".$adres_zamieszkania."','".$id_numer_kierunkowy."','".$numer_telefonu."','".$adres_e_mail."','".$PESEL."','".$numer_umowy."','".$numer_ubezpieczenia."','".$data_urodzenia."','".$data_zatrudnienia."','".$wynagrodzenie."','".$zdjecie."','".$id_stanowisko."','".$id_lokalizacja_pracy."','".$id_dzial."','".$id_samochod."',);";
+        //     $data = mysqli_query($this->connect, $query);
+        //     header('location: ../BO/pracownicy.php'); 
+        //     $this->close();
+        // }
 
-        function deletePracownik($id_pracownik){
-            $query = "Delete from pracownicy where id_pracownik =".$id_pracownik.";";
+        function deletePracownik($id_employee){
+            $query = "Delete from employees where id_employee =".$id_employee.";";
             $data = mysqli_query($this->connect, $query);
             unset($_GET['id']);
             header('location: ../BO/pracownicy.php'); 
             $this->close();
         }
 
-        function updatePracownik($id_pracownik, $imie, $nazwisko, $adres_zamieszkania, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail, $PESEL, $numer_umowy, $numer_ubezpieczenia, $data_urodzenia, $data_zatrudnienia, $wynagrodzenie, $zdjecie, $id_stanowisko, $id_lokalizacja_pracy, $id_dzial, $id_samochod){
-            $query = "UPDATE `pracownicy` SET `imie`='".$imie."',`nazwisko`='".$nazwisko."',`adres_zamieszkania`='".$adres_zamieszkania."',`id_numer_kierunkowy`='".$id_numer_kierunkowy."',`numer_telefonu`='".$numer_telefonu."',`adres_e_mail`='".$adres_e_mail."',`PESEL`='".$PESEL."',`numer_umowy`='".$numer_umowy."',`numer_ubezpieczenia`='".$numer_ubezpieczenia."',`data_urodzenia`='".$data_urodzenia."',`data_zatrudnienia`='".$data_zatrudnienia."',`wynagrodzenie`='".$wynagrodzenie."',`zdjecie`='".$zdjecie."',`id_stanowisko`='".$id_stanowisko."',`id_lokalizacja_pracy`='".$id_lokalizacja_pracy."',`id_dzial`='".$id_dzial."',`id_samochod`='".$id_samochod.";";
-            $data = mysqli_query($this->connect, $query);
-            unset($_GET['id']);
-            header('location: ../BO/pracownicy.php');  
-            $this->close();
-        }
+        // function updatePracownik($id_pracownik, $imie, $nazwisko, $adres_zamieszkania, $id_numer_kierunkowy, $numer_telefonu, $adres_e_mail, $PESEL, $numer_umowy, $numer_ubezpieczenia, $data_urodzenia, $data_zatrudnienia, $wynagrodzenie, $zdjecie, $id_stanowisko, $id_lokalizacja_pracy, $id_dzial, $id_samochod){
+        //     $query = "UPDATE `pracownicy` SET `imie`='".$imie."',`nazwisko`='".$nazwisko."',`adres_zamieszkania`='".$adres_zamieszkania."',`id_numer_kierunkowy`='".$id_numer_kierunkowy."',`numer_telefonu`='".$numer_telefonu."',`adres_e_mail`='".$adres_e_mail."',`PESEL`='".$PESEL."',`numer_umowy`='".$numer_umowy."',`numer_ubezpieczenia`='".$numer_ubezpieczenia."',`data_urodzenia`='".$data_urodzenia."',`data_zatrudnienia`='".$data_zatrudnienia."',`wynagrodzenie`='".$wynagrodzenie."',`zdjecie`='".$zdjecie."',`id_stanowisko`='".$id_stanowisko."',`id_lokalizacja_pracy`='".$id_lokalizacja_pracy."',`id_dzial`='".$id_dzial."',`id_samochod`='".$id_samochod.";";
+        //     $data = mysqli_query($this->connect, $query);
+        //     unset($_GET['id']);
+        //     header('location: ../BO/pracownicy.php');  
+        //     $this->close();
+        // }
     }
 ?>
