4737fc6aebe6d21f0a85ed74150cdfc602be0a8c
mostekd <dawid-mostowski@o2.pl>
2024-11-20 17:07:16 +0100

    update

---
+++ BO/admin_websites.php
@@ -0,0 +1,85 @@
+<?php
+    include_once ('../include/functions.php');
+
+    if ($id_employee == 0){
+        header('Location: ../FO/account.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./admin.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="./admin.js" defer></script>
+    <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="../images/ikona.png">
+    <title>Secur IT | Admin - Strony Internetowe</title>
+</head>
+<body>
+    <div class="tlo"></div>
+    <main class="main">
+        <?php
+            include("./admin_header.php");
+            include("./admin_nav.php");
+            // include("footer_admin.php");
+
+            include('../DB/db_services.php');
+            $baza = new db_services();
+        
+            if(!empty($_GET)){
+                $baza->databaseConnect();
+                if(isset($_GET['del']))
+                {
+                    $id_service=$_GET['id_service'];
+                    $baza->deleteServices($id_service);
+                }
+                if(isset($_GET['opcja'])){
+                    if($_GET['opcja'] == 'dodaj'){
+                        $id_service_type = 4;
+                        $name = $_GET['name'];
+                        $description = $_GET['description'];
+                        $price = $_GET['price'];
+                        $comment = $_GET['comment'];
+                        $baza->insertServices ($id_service_type, $name, $description, $price);
+                    }
+                    elseif($_GET['opcja'] == 'edytuj'){
+                        $id_service = $_GET['id_service'];
+                        $name = $_GET['name'];
+                        $description = $_GET['description'];
+                        $price = $_GET['price'];
+                        $baza->updateServices ($id_service, $name, $description, $price);
+                    }
+                }
+                else{
+                    echo "<p>Usługi nie ma w naszej bazie</p>";
+                }
+            }
+            
+            $baza->databaseConnect();
+            $data = $baza->selectServices_websites();
+            if (!empty($data)){ 
+        ?>
+            <div class="services">
+                <?php
+                    while($row = mysqli_fetch_assoc($data))
+                    {
+                        echo "<div id='service' class='service'>Nazwa: ".$row['name']." Opis: ".$row['description']." Cena: ".$row['price']."
+                        <button class='delete'><a href=admin_websites.php?del=True&id_service=".$row['id_service'].">
+                        Usuń usługę
+                        </a></button>
+                        <button class='delete'><a href=websites_edit.php?id_service=".$row['id_service'].">
+                        Edytuj usługę
+                        </a></button>
+                        </div>";
+                    }
+                    } else {
+                        echo "Brak Usług";
+                    }
+                    $baza->close();
+                ?>
+            </div>
+        </main>
+    </body>
+</htm>
---
+++ BO/databases_edit.php
@@ -0,0 +1,50 @@
+<?php
+    include_once ('../include/functions.php');
+
+    if ($id_employee == 0){
+        header('Location: ../FO/account.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./admin.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="./admin.js" defer></script>
+        <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
+        <link rel="shortcut icon" href="../images/ikona.png">
+        <title>Secur IT | Admin - Edytuj Bazy Danych</title>
+    </head>
+    <body>
+        <div class="tlo"></div>
+        <main class="main">
+            <?php
+                include("./admin_header.php");
+                include("./admin_nav.php"); 
+                include('../DB/db_services.php');
+                $baza = new db_services();
+            
+                if(!empty($_GET)){                
+                    $baza->databaseConnect();
+                    $id_service=$_GET['id_service'];
+                    $data = $baza->selectServiceById($id_service);
+                    if (!empty($data)){
+                        while($row = mysqli_fetch_assoc($data))
+                        {
+                            echo "<form class='MyForm' action='./admin_databases.php' method = 'get'>
+                                <input type=text name='name' placeholder='nazwa' id='name' class='name' value=".$row['name']."></input>
+                                <textarea type=text name='description' placeholder='opis' id='description' class='description' value=".$row['description']."></textarea>
+                                <input type=number name='price' placeholder='cena' id='price' class='price' value=".$row['price']."></input>";
+                            echo "<input type=hidden name='id_service' id='id_service' class='id_service' value=".$row['id_service']."></input>
+                                <input type=hidden name='opcja' id='opcja' class='opcja' value='edytuj'></input>
+                                <input type='submit'></input>
+                                </form>";
+                        }
+                    }
+                }
+            ?>
+        </main>        
+    </body>
+</html>
