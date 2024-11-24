131ab4c7f544455d77df8a543beefa1acc32a5a8
mostekd <dawid-mostowski@o2.pl>
2024-11-24 08:41:40 +0100

    Update functions.php

--- include/functions.php
+++ include/functions.php
@@ -7,9 +7,10 @@
         $id_user = $_SESSION['id_user'];
         $is_admin = $_SESSION['is_admin'];
         $id_employee = $_SESSION['id_employee'];
-        $id_company -= $_SESSION['id_company'];
+        $id_company = $_SESSION['id_company'];
     }
     else {
+        $id_company = 0;
         $id_employee = 0;
         $id_user = 0;
         $is_admin  = 0;
